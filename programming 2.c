3d2c7e4a8bd58d7bde7323002ca1d7ca0b725190
Anikk1234 <anikkirtania9@gmail.com>
2024-04-07 00:54:44 +0600

    first commit

---
+++ programming 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("Please Inter Two Numbers:");
+  scanf("%d %d\n",&num1,&num2);
+  sum=num1 + num2;
+  printf("Total:%d\n",sum);
+  return 0;
+
+}
+
